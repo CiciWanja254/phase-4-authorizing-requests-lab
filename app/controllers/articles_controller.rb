238d75e4ba31903fe504520827602f59877d217d
Cici254 <thukucindy00@gmail.com>
2023-07-05 12:51:33 +0300

    Complete assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
-
+  
   def show
     article = Article.find(params[:id])
     render json: article
